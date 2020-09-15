696c9c10e6db78b55e3a80246dfde889768b6d87
Sergey Sergeychik <sergey.sergeychik@gmail.com>
2020-09-15 11:39:29 +0300

    Решение

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+     int ticket = 10623;
+     int spent = 20;
+     int miles = 1;
+     int bonus = ticket / spent * miles;
+     System.out.println(bonus);
+    }
+
+
+
+
+}
